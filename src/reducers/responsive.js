e29528b28dad22bddac7299b7c55c5b43db040b3
Dapeng Zhang <dapengzhang1992@gmail.com>
2019-08-05 14:29:06 +1000

    Reducer - to dispatch viewportWidth when window resized

--- src/reducers/responsive.js
+++ src/reducers/responsive.js
@@ -1,7 +1,7 @@
 const initState = {
     viewportWidth: window.innerWidth
 }
-
+ 
 //to be dispatched by 'PersonalWebsite'
 //state to be used by 'responsiveWrapper' 
 export default function(state = initState, action){
