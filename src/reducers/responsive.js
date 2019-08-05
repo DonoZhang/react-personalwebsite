d3f2f56d9d1e9d628114b967fd8182f945aa846c
Dapeng Zhang <dapengzhang1992@gmail.com>
2019-08-05 16:58:26 +1000

    The reducer - initialize and support dispatch of 'viewportWidth' state for redux

--- src/reducers/responsive.js
+++ src/reducers/responsive.js
@@ -1,14 +1,14 @@
 const initState = {
-    viewportWidth: window.innerWidth
+	viewportWidth: window.innerWidth
 }
  
 //to be dispatched by 'PersonalWebsite'
 //state to be used by 'responsiveWrapper' 
 export default function(state = initState, action){
-    switch(action.type){
-        case 'UPDATE_VIEWPORT_WIDTH': 
-            return {...state, viewportWidth: action.viewportWidth}
-        default:
-            return state
-    }
+	switch(action.type){
+		case 'UPDATE_VIEWPORT_WIDTH': 
+			return {...state, viewportWidth: action.viewportWidth}
+		default:
+			return state
+	}
 }
